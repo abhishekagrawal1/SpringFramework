c11b90137ccab0d298e0eddeba195ce639f58eaf
abhishekagrawal <abhishekagrawal@ebay.com>
2017-08-31 18:11:18 +0530

    Basic Spring Framework Example

---
+++ src/com/abhishek/learning/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.abhishek.learning;
+
+public class HelloWorld {
+    private String message;
+
+    public String getMessage() {
+        return "Message: " + message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
