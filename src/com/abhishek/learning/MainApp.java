1790670e1f763dbed0121655cb9b17b0a69572be
abhishekagrawal <abhishekagrawal@ebay.com>
2017-09-02 20:51:46 +0530

    Spring BeanFactory Container and ApplicationContext container

--- src/com/abhishek/learning/MainApp.java
+++ src/com/abhishek/learning/MainApp.java
@@ -1,12 +1,22 @@
 package com.abhishek.learning;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
     public static void main(String args[]){
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        BeanFactory context = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("./src/beans.xml");
         HelloWorld object = (HelloWorld) context.getBean("helloWorld");
+        System.out.println("With Bean Factory");
         System.out.println(object.getMessage());
+
+        HelloWorld object1 = (HelloWorld) applicationContext.getBean("helloWorld");
+
+        System.out.println("With Application Context Container");
+        System.out.println(object1.getMessage());
     }
 }
